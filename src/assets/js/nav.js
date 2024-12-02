567ec8653da0b54b155770cb2da53186638ff7b3
Larss830 <larence830@gmail.com>
2024-12-03 01:50:35 +0800

    fix logo size and add custom colors

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -22,3 +22,14 @@ function ariaExpanded() {
         csUL.setAttribute("aria-expanded", "false");
     }
 }
+
+
+
+document.addEventListener('scroll', (e) => { 
+    const scroll = document.documentElement.scrollTop;
+    if(scroll >= 100){
+document.querySelector('body').classList.add('scroll')
+    } else {
+    document.querySelector('body').classList.remove('scroll')
+    }
+});
