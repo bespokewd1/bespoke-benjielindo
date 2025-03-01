da112fcbdc9a197161df7a02c15d3ce3f939dd48
churchilldotexe <churchillexe@gmail.com>
2025-03-02 07:43:01 +0800

    chore: change image background bbase on the description and adjust height

--- src/assets/js/promotion-gallery.js
+++ src/assets/js/promotion-gallery.js
@@ -4,13 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
    const promoImages = gallery.querySelectorAll(".promo-img");
 
-   promoImages.forEach(img => {
+   promoImages.forEach((img, index) => {
       const measuringImg = new Image();
       measuringImg.src = img.src;
       measuringImg.onload = function() {
          const width = measuringImg.naturalWidth;
          const height = measuringImg.naturalHeight;
          const layout = width > height ? "landscape" : "portrait";
+
+         // TODO: check if im the last in the index, if not,
+         // check if im landscape if yes, 
+         // add to tracker (this tracker will store consecutive landscape
+         // until it hits the last child that is also a landscape , 
+         // TRACKER will reset the moment that the current **img** 
+         // is portrait >>>> clear tracker back to 0 and start again 
+         // until last child.
 
          const parentItem = img.closest(".cs-item");
          if (parentItem) {
@@ -23,4 +31,13 @@ document.addEventListener("DOMContentLoaded", function() {
          console.error(`Failed to load image: ${img.src}`);
       };
    });
+
+   // TODO: 
+   // check if tracker has a value (length)
+   // return if none 
+   // if tracker has a value, setAttribute "data-span" to all the children 
+   //
+   //
+   //
+   // think of something else. its a bust 
 });
